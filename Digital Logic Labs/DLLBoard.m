8a008997e8bb107bdadb294fb41bbfd5c30b4b44
Joe Scharnitzke <joe@dijkstra.cslab.pepperdine.edu>
2013-04-15 16:49:35 -0700

    Changed a word in a comment.

--- Digital Logic Labs/DLLBoard.m
+++ Digital Logic Labs/DLLBoard.m
@@ -487,7 +487,7 @@ DLLAComponent * breadboardStateArray[NUMCOLUMNS][NUMROWS];
     return occupied;
     
     // If a chip or a wire is at given coords return occupied (YES)
-    // (99,99) is a 'trash' point, items put here will not be added-mark as always unavailable
+    // (99,99) is a 'trash' point, items put here will not be added-mark as always available
 }
 
 - (DLLAComponent *)boardStateAt:(DLLPoint *)coords
